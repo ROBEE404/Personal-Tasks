f91a49065c025588d971b175b6277bce553d8aba
MR.ROBEE <111158704+ROBEE404@users.noreply.github.com>
2026-01-31 01:28:09 +0300

    apk app

---
+++ src-tauri/gen/android/app/src/main/java/com/robee/tasks/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.robee.tasks
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,23 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-})
+
+ clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell Vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+});
